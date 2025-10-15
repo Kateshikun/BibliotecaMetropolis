c38995b7c1239392967f2ff6ed1bbe28532dedd4
Kateshikun <danieladriancastillogarcia@gmail.com>
2025-10-15 08:27:31 -0600

    Agregar archivos de proyecto.

---
+++ BibliotecaMetrópolis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BibliotecaMetrópolis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
