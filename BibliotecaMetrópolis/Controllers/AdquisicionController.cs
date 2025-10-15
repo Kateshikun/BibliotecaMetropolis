862ade866ebdf35eaee1038ce1f8deba3a1c152b
Kateshikun <danieladriancastillogarcia@gmail.com>
2025-10-15 17:50:16 -0600

    Parte 1 de Adquisicion de Recursos

---
+++ BibliotecaMetrópolis/Controllers/AdquisicionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaMetrópolis.Controllers
+{
+    public class AdquisicionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
